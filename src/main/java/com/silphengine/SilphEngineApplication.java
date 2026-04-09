bf8cb4d6291c8693c6d68a9a93a6382f6452e67a
cadimodev <cadimodev@gmail.com>
2026-04-09 11:26:25 +0200

    initial project setup with Spring Boot 4 and Java 25
    Generated project with dependencies: JPA, Security, Lombok, Validation, Postgres, and WebFlux. Configured for Java 25 and YAML

---
+++ src/main/java/com/silphengine/SilphEngineApplication.java
@@ -0,0 +1,13 @@
+package com.silphengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilphEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilphEngineApplication.class, args);
+	}
+
+}
