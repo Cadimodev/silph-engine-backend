24bf53a4e109d5137ed53a6bf23979703dd112d9
cadimodev <cadimodev@gmail.com>
2026-05-19 11:20:03 +0200

    feat: Dockerize application with Docker Compose

--- src/main/java/com/silphengine/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/silphengine/infrastructure/config/OpenApiConfig.java
@@ -1,33 +1,29 @@
 package com.silphengine.infrastructure.config;
 
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI silphEngineOpenAPI() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .name("JWT Authentication");
-
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
         return new OpenAPI()
                 .info(new Info().title("Silph Engine API")
                         .description("REST API for managing and synchronizing Pokémon TCG cards")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .addSecurityItem(securityRequirement);
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
     }
 }
